ffe17f026abd0877cbb991d7708e4802b4ba8a2d
hirusha-adi <36286877+hirusha-adi@users.noreply.github.com>
2024-07-23 22:16:14 +0530

    Update default.vue

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,13 +1,16 @@
 <template>
   <v-app>
 
+    <!-- Navbar -->
     <AppNav />
 
     <v-main>
       <router-view />
     </v-main>
 
+    <!-- Footer -->
     <AppFooter />
+
   </v-app>
 </template>
 
