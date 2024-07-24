72311f5f95ccd465034db662980386e21a526b10
hirusha-adi <36286877+hirusha-adi@users.noreply.github.com>
2024-07-24 21:58:43 +0530

    tmp works

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,8 +1,38 @@
+<script setup>
+import { ref, watch } from 'vue'
+
+const items = [
+    {
+        title: 'Foo',
+        value: 'foo',
+    },
+    {
+        title: 'Bar',
+        value: 'bar',
+    },
+    {
+        title: 'Fizz',
+        value: 'fizz',
+    },
+    {
+        title: 'Buzz',
+        value: 'buzz',
+    },
+]
+
+const drawer = ref(false)
+const group = ref(null)
+
+watch(group, () => {
+    drawer.value = false
+})
+</script>
+
 <template>
     <div>
         <v-app-bar :elevation="1">
             <template v-slot:prepend>
-                <v-app-bar-nav-icon></v-app-bar-nav-icon>
+                <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
             </template>
 
             <v-app-bar-title>is it healthy?</v-app-bar-title>
@@ -15,5 +45,9 @@
                 <v-btn icon="mdi-dots-vertical"></v-btn>
             </template>
         </v-app-bar>
+
+        <v-navigation-drawer v-model="drawer" :location="$vuetify.display.mobile ? 'bottom' : undefined" temporary>
+            <v-list :items="items"></v-list>
+        </v-navigation-drawer>
     </div>
 </template>
