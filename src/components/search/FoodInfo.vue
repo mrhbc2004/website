5c928428fee5e2587e36fa08293bbb65b45587d3
hirusha-adi <36286877+hirusha-adi@users.noreply.github.com>
2024-07-24 15:57:14 +0530

    also show more other stuff

--- src/components/search/FoodInfo.vue
+++ src/components/search/FoodInfo.vue
@@ -1,6 +1,7 @@
 <script setup>
 
 import { ref } from 'vue'
+import { fa } from 'vuetify/locale';
 
 const props = defineProps({
     inscode: Object,
@@ -11,6 +12,10 @@ const show = ref(
         origin: false,
         daily_intake: false,
         side_effects: true,
+        uses: false,
+        precautions: false,
+        overview: false,
+        interactions: false,
     }
 )
 
@@ -50,6 +55,61 @@ const formattedContent = (key) => {
             <div v-show="show.side_effects">
                 <v-divider></v-divider>
                 <v-card-text v-html="formattedContent('side_effects')"></v-card-text>
+            </div>
+        </v-expand-transition>
+
+        <!-- Overview -->
+        <v-card-actions class="highlight-on-hover">
+            <p class="ml-2 font-weight-bold text-h6">Overview</p>
+            <v-spacer></v-spacer>
+            <v-btn :icon="show.overview ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+                @click="show.overview = !show.overview"></v-btn>
+        </v-card-actions>
+        <v-expand-transition>
+            <div v-show="show.overview">
+                <v-divider></v-divider>
+                <v-card-text v-html="formattedContent('overview')"></v-card-text>
+            </div>
+        </v-expand-transition>
+
+        <!-- Uses -->
+        <v-card-actions class="highlight-on-hover">
+            <p class="ml-2 font-weight-bold text-h6">Uses</p>
+            <v-spacer></v-spacer>
+            <v-btn :icon="show.uses ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show.uses = !show.uses"></v-btn>
+        </v-card-actions>
+        <v-expand-transition>
+            <div v-show="show.uses">
+                <v-divider></v-divider>
+                <v-card-text v-html="formattedContent('uses')"></v-card-text>
+            </div>
+        </v-expand-transition>
+
+        <!-- Precautions -->
+        <v-card-actions class="highlight-on-hover">
+            <p class="ml-2 font-weight-bold text-h6">Precautions</p>
+            <v-spacer></v-spacer>
+            <v-btn :icon="show.precautions ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+                @click="show.precautions = !show.precautions"></v-btn>
+        </v-card-actions>
+        <v-expand-transition>
+            <div v-show="show.precautions">
+                <v-divider></v-divider>
+                <v-card-text v-html="formattedContent('precautions')"></v-card-text>
+            </div>
+        </v-expand-transition>
+
+        <!-- Interactions -->
+        <v-card-actions class="highlight-on-hover">
+            <p class="ml-2 font-weight-bold text-h6">Interactions</p>
+            <v-spacer></v-spacer>
+            <v-btn :icon="show.interactions ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+                @click="show.interactions = !show.interactions"></v-btn>
+        </v-card-actions>
+        <v-expand-transition>
+            <div v-show="show.interactions">
+                <v-divider></v-divider>
+                <v-card-text v-html="formattedContent('interactions')"></v-card-text>
             </div>
         </v-expand-transition>
 
